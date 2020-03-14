ba8929ce97a5f587d59808a4d7e246fab271f8a1
bright_liang_liang <1819345503@qq.com>
2020-03-14 22:37:08 +0800

    python学习到达递归

--- 从零开始学python/随手练习文件/test.py
+++ 从零开始学python/随手练习文件/test.py
@@ -1,6 +1,14 @@
-for i in range(10):
-    print(str(i))
-    if i == 5:
-        break
-else:
-    print("会执行么？")
+'''
+@Descripttion: 
+@version: 
+@Author: 亮亮
+@Date: 2020-03-12 19:58:37
+@LastEditors: 亮亮
+@LastEditTime: 2020-03-14 22:21:08
+'''
+def news(num):
+    print(num)
+    if num == 1:
+        return
+    news(num - 1)
+news(10)    
