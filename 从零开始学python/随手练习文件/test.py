c3eeeb1106742ba729dd9b02b2a402197a1278a7
bright_liang_liang <1819345503@qq.com>
2020-03-15 17:47:11 +0800

    面向对象

--- 从零开始学python/随手练习文件/test.py
+++ 从零开始学python/随手练习文件/test.py
@@ -1,14 +1,1 @@
-'''
-@Descripttion: 
-@version: 
-@Author: 亮亮
-@Date: 2020-03-12 19:58:37
-@LastEditors: 亮亮
-@LastEditTime: 2020-03-14 22:21:08
-'''
-def news(num):
-    print(num)
-    if num == 1:
-        return
-    news(num - 1)
-news(10)    
+a = 
