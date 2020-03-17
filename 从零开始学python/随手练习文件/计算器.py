8e5ad5ba834070c773e9ace1608706610e7549cf
bright_liang_liang <1819345503@qq.com>
2020-03-17 21:53:27 +0800

    简易版本计算器

---
+++ 从零开始学python/随手练习文件/计算器.py
@@ -0,0 +1,12 @@
+'''
+@Author: your name
+@Date: 2020-03-17 21:18:24
+@LastEditTime: 2020-03-17 21:19:21
+@LastEditors: Please set LastEditors
+@Description: In User Settings Edit
+@FilePath: \giee\learn_python\从零开始学python\随手练习文件\计算器.py
+'''
+while True:
+
+    input_str = input("请输入要计算的公式：")
+    print(eval(input_str))
